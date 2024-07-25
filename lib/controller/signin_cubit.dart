443ef063fca8b8366c1b207a6521b99dd1a27326
kiranIssac <64491888+kiranIssac@users.noreply.github.com>
2024-07-25 13:59:43 +0100

    bloc
    update business logic bloc

--- lib/controller/signin_cubit.dart
+++ lib/controller/signin_cubit.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class SignInCubit extends Cubit<bool> {
-  SignInCubit() : super(false);
+class SignInCubit extends Cubit<String> {
+  SignInCubit() : super('');
 
   void signIn(String username, String password) {
     // Replace this with actual sign-in logic
     if (username == 'e' && password == 'p') {
       
-      emit(true); // Sign-in successful
+      emit('Success'); // Sign-in successful
     } else {
-      emit(false); // Sign-in failed
+      emit('Sign in Failed'); // Sign-in failed
     }
   }
 }
--- lib/view/signIn.dart
+++ lib/view/signIn.dart
@@ -6,35 +6,52 @@ import 'package:flutter_application_2/view/my_collection.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignIn extends StatelessWidget {
-   SignIn({super.key});
+  SignIn({super.key});
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+    Color colorStatus=Colors.red;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: const Color(0xff122329),
-      appBar: AppBar(backgroundColor: const Color(0xff122329),foregroundColor: Colors.white,),
+    return Scaffold(
+      backgroundColor: const Color(0xff122329),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff122329),
+        foregroundColor: Colors.white,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Align(
-              alignment: Alignment.centerLeft,
-              child: Text('Sign In',style: TextStyle(fontSize: 22, color: Colors.white))),
-              const SizedBox(height: 35,),
-            TextField(style: const TextStyle(color: Colors.white),
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Email',labelStyle: TextStyle(color: Color(0xffD49A00)),),
+                alignment: Alignment.centerLeft,
+                child: Text('Sign In',
+                    style: TextStyle(fontSize: 22, color: Colors.white))),
+            const SizedBox(
+              height: 35,
             ),
-            TextField(style: const TextStyle(color: Colors.white),
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Color(0xffD49A00)),
+              ),
+            ),
+            TextField(
+              style: const TextStyle(color: Colors.white),
               controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password',labelStyle: TextStyle(color: Color(0xffD49A00))),
+              decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Color(0xffD49A00))),
               obscureText: true,
             ),
             const SizedBox(height: 20),
-                   BlocConsumer<SignInCubit, bool>(
-              listener: (context, signedIn) {
-                if (signedIn) {
+            BlocConsumer<SignInCubit, String>(
+              listener: (context, status) {
+              
+                if (status == 'Success') {
+                   colorStatus=Colors.green;
                   Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(builder: (context) => MyCollection()),
@@ -42,24 +59,26 @@ class SignIn extends StatelessWidget {
                 }
               },
               builder: (context, signedIn) {
-                return signedIn
-                    ? const Text('Sign-in Successful', style: TextStyle(color: Colors.green))
-                    : const Text('Sign-in Failed', style: TextStyle(color: Colors.red));
+                return Text(signedIn, style: TextStyle(color: colorStatus));
               },
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-               style: const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Color(0xffD49A00))),
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Color(0xffD49A00))),
               onPressed: () {
                 final username = _usernameController.text;
                 final password = _passwordController.text;
                 context.read<SignInCubit>().signIn(username, password);
               },
               child: const Padding(
-                padding: EdgeInsets.only(left: 40,right: 40),
-                child: Text('Continue', style: TextStyle(fontSize: 16, color: Colors.black,fontWeight: FontWeight.bold,)),
+                padding: EdgeInsets.only(left: 40, right: 40),
+                child: Text('Continue',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    )),
               ),
             ),
           ],
